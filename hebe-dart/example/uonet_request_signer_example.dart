8ee42b0f3bd019ea70727cf6cc72665d30f27808
Marceeli <Marceli.f@outlook.com>
2024-12-07 13:39:07 +0100

    Delete hebe-dart directory

--- hebe-dart/example/uonet_request_signer_example.dart
+++
@@ -1,5 +0,0 @@
-import 'package:uonet_request_signer/uonet_request_signer.dart';
-
-void main() {
-  getSignatureValues(fingerprint, privateKey, body, fullUrl, DateTime.now());
-}
--- hebe-dart/lib/uonet_request_signer.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library uonet_request_signer;
-
-export 'src/uonet_request_signer_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
